513e8c8018650af937efa206d7b95f78bd85eb41
Sanyam-Shandilya <sanyamshandilya8@gmail.com>
2024-01-29 03:35:58 +0530

    updated the code to have a better navbar and added a hero image with some text

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 console.log("hi");
 var toogle = document.getElementById("toogle");
 var body = document.querySelector("body");
+var jumbo = document.getElementsByClassName("navbar");
+
 
 toogle.addEventListener("click", () => {
     var img_toogle = toogle.querySelector("img");
@@ -8,10 +10,17 @@ toogle.addEventListener("click", () => {
         img_toogle.src = "images/moon.png";
         body.classList.remove("c-light");
         body.classList.add("c-dark");
+        for (var i = 0; i < jumbo.length; i++) {
+            jumbo[i].style.backgroundColor = '#264653';
+            jumbo[i].style.color = "#FEFAE0";
+        }
     }else{
         img_toogle.src = "images/sun.png";
         body.classList.add("c-light");
         body.classList.remove("c-dark");
+        for (var i = 0; i < jumbo.length; i++) {
+            jumbo[i].style.backgroundColor = "#FEFAE0";
+            jumbo[i].style.color = '#264653';
+        }
     }
-    console.log(body.classList);
 });
